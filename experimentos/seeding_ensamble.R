f2b59ebf2d93e0840230d82787e8d96d505aff8a
martinaweil <martinaweil@hotmail.com>
2023-11-09 01:24:27 +0000

    submuestreo de semillas

--- experimentos/seeding_ensamble.R
+++ experimentos/seeding_ensamble.R
@@ -1,65 +1,77 @@
-
-#limpio la memoria
-rm(list = ls()) # remove all objects
-gc() # garbage collection
+# Limpieza inicial
+rm(list = ls())
+gc()
 library(data.table)
 
 initial_seed <- 100019
 num_seeds <- 30
 seeds_to_ensemble <- seq(initial_seed, initial_seed + num_seeds - 1)
-
-# leemos las predicciones que hicimos
-seed <- seeds_to_ensemble[1]  # Choose the first seed
-predicted_prob_file <- paste0("prediccion_", seed, ".txt")
-ensemble_results <- fread(predicted_prob_file, sep = "\t", header = TRUE)
-
-
-setnames(ensemble_results, "prob", paste0("prob_", seed))
-
-# Loop sobre las semillas
-for (i in 2:length(seeds_to_ensemble)) {  
-  seed <- seeds_to_ensemble[i]
+# Función para realizar un ensamble con un conjunto de semillas específico
+realizar_ensamble <- function(seed_indices) {
+  # Leer las predicciones que hiciste
+  seed <- seeds_to_ensemble[seed_indices[1]]  # Choose the first seed
   predicted_prob_file <- paste0("prediccion_", seed, ".txt")
-  predicted_prob <- fread(predicted_prob_file, sep = "\t", header = TRUE)
+  ensemble_results <- fread(predicted_prob_file, sep = "\t", header = TRUE)
+  setnames(ensemble_results, "prob", paste0("prob_", seed))
   
- 
-  setnames(predicted_prob, "prob", paste0("prob_", seed))
+  # Loop sobre las semillas
+  for (i in 2:length(seed_indices)) {
+    setwd("~/buckets/b1/exp/exp_1")
+    seed <- seeds_to_ensemble[seed_indices[i]]
+    predicted_prob_file <- paste0("prediccion_", seed, ".txt")
+    predicted_prob <- fread(predicted_prob_file, sep = "\t", header = TRUE)
+    
+    setnames(predicted_prob, "prob", paste0("prob_", seed))
+    
+    # Merge en base al numero_de_cliente y foto_mes
+    ensemble_results <- merge(ensemble_results, predicted_prob, by = c("numero_de_cliente", "foto_mes"), all = TRUE)
+    
+    # Promedio las probabilidades
+    ensemble_results[, ensemble_prob := rowMeans(.SD, na.rm = TRUE), .SDcols = grep("prob_", names(ensemble_results))]
+    
+    ensemble_results[, grep("prob_", names(ensemble_results)) := NULL]
+  }
   
-  # mergeo en base al  numero_de_cliente y foto_mes
-  ensemble_results <- merge(ensemble_results, predicted_prob, by = c("numero_de_cliente", "foto_mes"), all = TRUE)
+  # Guardo el resultado
+  fwrite(ensemble_results, file = "ensemble_results.txt", sep = "\t")
+  setorder(ensemble_results, -ensemble_prob)
   
-  # promedio las prob
-  ensemble_results[, ensemble_prob := rowMeans(.SD, na.rm = TRUE), .SDcols = grep("prob_", names(ensemble_results))]
+  ensemble_results[, Predicted := 0L]
+  ensemble_results[1:11000, Predicted := 1L]
   
+  # Cálculo de ganancia. Envío estímulos a los primeros 11,000 en el ranking
+  setwd("~/buckets/b1")
+  datos <- fread("./datasets/competencia_03.csv.gz", sep = ",", header = TRUE)
+  datos <- datos[foto_mes == '202107']
   
-  ensemble_results[, grep("prob_", names(ensemble_results)) := NULL]
+  setwd("~/buckets/b1/exp/exp_1")
+  merged_data <- merge(ensemble_results, datos, by = c("foto_mes", "numero_de_cliente"), all.x = TRUE)
+  merged_data <- merged_data[, c("foto_mes", "numero_de_cliente", "clase_ternaria", "Predicted"), with = FALSE]
+  
+  merged_data[, ganancia := ifelse(Predicted == 1 & clase_ternaria == "BAJA+2", 273000,
+                                   ifelse(Predicted == 1 & clase_ternaria != "BAJA+2", -7000, 0))]
+  
+  total_gain <- sum(merged_data$ganancia)
+  return(total_gain)
 }
 
-# guardo el resultado
-fwrite(ensemble_results, file = "ensemble_results.txt", sep = "\t")
-setorder(ensemble_results, -ensemble_prob)
+setwd("~/buckets/b1/exp/exp_1")
+# Número de ensambles
+num_ensambles <- 10
+ensamble_results <- numeric(num_ensambles)
 
+# Realiza 10 ensambles con submuestras de 20 semillas
+for (ensamble_idx in 1:num_ensambles) {
+  # Submuestreo aleatorio de 20 semillas
+  sampled_seeds <- sample(1:num_seeds, 20)
+  
+  # Realiza el ensamble y almacena la ganancia
+  ensamble_results[ensamble_idx] <- realizar_ensamble(sampled_seeds)
+}
 
-ensemble_results[, Predicted := 0L]
-ensemble_results[1:11000, Predicted := 1L]
+# Ahora, ensamble_results contiene las ganancias para cada ensamble
 
-#calculo de ganancia. Envio estimulos a los primeros 11mil en el ranking
-
-datos<-fread("./datasets/competencia_03.csv.gz", sep = ",", header = TRUE) 
-datos <- datos[foto_mes == '202107']
-
-
-merged_data <- merge(ensemble_results, datos, by = c("foto_mes", "numero_de_cliente"), all.x = TRUE)
-merged_data <- merged_data[, c("foto_mes", "numero_de_cliente", "clase_ternaria","Predicted"), with = FALSE]
-
-
-
-
-merged_data[, ganancia := ifelse(Predicted == 1 & clase_ternaria == "BAJA+2", 273000,
-                                 ifelse(Predicted == 1 & clase_ternaria != "BAJA+2", -7000, 0))]
-
-
-total_gain <- sum(merged_data$ganancia)
+# Puedes comparar los resultados o realizar el análisis que desees con ensamble_results.
 
 
 
